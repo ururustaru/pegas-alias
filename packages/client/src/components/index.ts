734fe93809035935512a71e516769b72002eb5c7
Sapronov Sergey <admin@stacksite.ru>
2022-10-03 23:17:03 +0300

    Добавлены новые стили кнопок, свертсана страница старта раунда

--- packages/client/src/components/index.ts
+++ packages/client/src/components/index.ts
@@ -3,3 +3,4 @@ export * from './field/field';
 export * from './form-field/form-field';
 export * from './intro/intro';
 export * from './page-navigation/page-navigation';
+export * from './breadcrumbs/breadcrumbs';
