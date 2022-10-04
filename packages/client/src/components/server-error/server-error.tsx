d37ddafa86eb1e0ade9cc44cd9ecc4ebb00d0766
gremwiz1 <gremwiz@yandex.ru>
2022-10-04 21:43:19 +0300

    added component server-error

---
+++ packages/client/src/components/server-error/server-error.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './server-error.scss'
+import logo from '../../assets/images/505.png'
+
+export interface IServerError {
+  logo?: string
+}
+
+export function ServerError(props: IServerError) {
+  return (
+    <div className="server-error">
+      <h1 className="server-error__title">Pegas Alias</h1>
+      <p className="server-error__desc">Угадывый слова по-новому</p>
+      <div className="server-error__image-wrap">
+        <img src={props.logo ?? logo} alt="Логотип Pegas Alias с единорогом" />
+      </div>
+      <h2 className="server-error__subtitle">Ошибка 505</h2>
+      <p className="server-error__text">Сервер прилёг, мы уже исправляем</p>
+    </div>
+  )
+}
