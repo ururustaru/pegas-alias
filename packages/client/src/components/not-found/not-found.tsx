2c4f56d05e22b5b2529d860411da3c306b515f7d
gremwiz1 <gremwiz@yandex.ru>
2022-10-08 01:56:25 +0300

    fix component notFound

--- packages/client/src/components/not-found/not-found.tsx
+++ packages/client/src/components/not-found/not-found.tsx
@@ -10,22 +10,20 @@ export const NotFound: FC = () => {
     navigate('/')
   }
   return (
-    <>
-      <div className="not-found">
-        <div className="not-found__backlink">
-          <BackLink text="В главное меню" link={link} />
-        </div>
-        <h1 className="not-found__title">Pegas Alias</h1>
-        <p className="not-found__desc">Угадывый слова по-новому</p>
-        <div className="not-found__image-wrap">
-          <img src={logo} alt="Логотип Pegas Alias с единорогом" />
-        </div>
-        <h2 className="not-found__subtitle">Ошибка 404</h2>
-        <p className="not-found__text">Такого адреса не существует</p>
-        <Link to={'/'} className="not-found__text not-found__text_link">
-          Вернуться к игре
-        </Link>
+    <div className="not-found">
+      <div className="not-found__backlink">
+        <BackLink text="В главное меню" link={link} />
       </div>
-    </>
+      <h1 className="not-found__title">Pegas Alias</h1>
+      <p className="not-found__desc">Угадывый слова по-новому</p>
+      <div className="not-found__image-wrap">
+        <img src={logo} alt="Логотип Pegas Alias с единорогом" />
+      </div>
+      <h2 className="not-found__subtitle">Ошибка 404</h2>
+      <p className="not-found__text">Такого адреса не существует</p>
+      <Link to={'/'} className="not-found__text not-found__text_link">
+        Вернуться к игре
+      </Link>
+    </div>
   )
 }
