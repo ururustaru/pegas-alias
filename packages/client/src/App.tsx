ed769f4042c3d6dafd31609214fdc7910b864447
gremwiz1 <gremwiz@yandex.ru>
2022-10-08 16:41:11 +0300

    added component ScoreInRound

--- packages/client/src/App.tsx
+++ packages/client/src/App.tsx
@@ -1,9 +1,21 @@
 // import { useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import PageNavigation from './components/page-navigation/page-navigation';
-import {Main, Login, SignUp, NewGame, Profile, Rules, Leaders, ForumPage, Round, ForumDetail} from './pages/';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import PageNavigation from './components/page-navigation/page-navigation'
+import {
+  Main,
+  Login,
+  SignUp,
+  NewGame,
+  Profile,
+  Rules,
+  Leaders,
+  ForumPage,
+  Round,
+  ForumDetail,
+  ScoreInRoundPage,
+} from './pages/'
 
-import './scss/style.scss';
+import './scss/style.scss'
 
 export function App() {
   // useEffect(() => {
@@ -17,23 +29,38 @@ export function App() {
   //   //
   //   // fetchServerData()
   // }, [])
-  return <div className="app">
-    <Router>
-      <PageNavigation />
-      <Routes>
-        <Route path="/" element={<Main/>}/>
-        <Route path="/login" element={<Login />}/>
-        <Route path="/sign-up" element={<SignUp />}/>
-        <Route path="/about" element={<Login/>}/>
-        <Route path="/new-game" element={<NewGame/>}/>
-        <Route path="/profile" element={<Profile/>}/>
-        <Route path="/rules" element={<Rules/>}/>
-        <Route path="/leaders" element={<Leaders/>}/>
-        <Route path="/forum" element={<ForumPage/>}/>
-        <Route path="/round" element={<Round />} />
-        <Route path="/forum-detail" element={<ForumDetail />}/>
-      </Routes>
-    </Router>
-  </div>
+  return (
+    <div className="app">
+      <Router>
+        <PageNavigation />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/about" element={<Login />} />
+          <Route path="/new-game" element={<NewGame />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/rules" element={<Rules />} />
+          <Route path="/leaders" element={<Leaders />} />
+          <Route path="/forum" element={<ForumPage />} />
+          <Route path="/round" element={<Round />} />
+          <Route
+            path="/score-in-round"
+            element={
+              <ScoreInRoundPage
+                numberRound={1} //Данные указаны для примера использования
+                nextNameTeam={'Настольные монстры'}
+                arrayScoreTeams={[
+                  { id: '1', nameTeam: 'Мудрые черепахи', scoreTeam: 15 },
+                  { id: '2', nameTeam: 'Настольные монстры', scoreTeam: 0 },
+                  { id: '3', nameTeam: 'Девочки', scoreTeam: 0 },
+                ]}
+              />
+            }
+          />
+          <Route path="/forum-detail" element={<ForumDetail />} />
+        </Routes>
+      </Router>
+    </div>
+  )
 }
-
--- packages/client/src/components/index.ts
+++ packages/client/src/components/index.ts
@@ -1,13 +1,14 @@
-export * from './button/button';
-export * from './field/field';
-export * from './form-field/form-field';
-export * from './intro/intro';
-export * from './page-navigation/page-navigation';
-export * from './back-link/back-link';
-export * from './checkbox/checkbox';
-export * from './range/range';
-export * from './leaderboard/leaderboard';
-export * from './forums/forums';
+export * from './button/button'
+export * from './field/field'
+export * from './form-field/form-field'
+export * from './intro/intro'
+export * from './page-navigation/page-navigation'
+export * from './back-link/back-link'
+export * from './checkbox/checkbox'
+export * from './range/range'
+export * from './leaderboard/leaderboard'
+export * from './forums/forums'
+export * from './score-in-round/score-in-round'
 
 // Modals
 export * from './modal/add-forum-modal'
