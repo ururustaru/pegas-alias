0fe729521ceac5620f61cc734ca17db6f4d16167
Sergey Sapronov <1262708@mail.ru>
2022-10-16 20:23:06 +0300

    new file:   packages/client/src/components/canvas/canvas.tsx 	modified:   packages/client/src/pages/round-process.tsx 	new file:   pegas-alias

--- packages/client/src/pages/round-process.tsx
+++ packages/client/src/pages/round-process.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import {Button, Timer} from '../components';
-
+import { CanvasComponent } from '../components/canvas/canvas';
 import './../scss/form/form.scss';
 import './../components/round/round.scss';
 
@@ -12,16 +12,6 @@ export class RoundProcess extends React.Component {
   intervalIDs:NodeJS.Timeout[] = [];
 
   componentDidMount() {
-    const canvas = document.getElementById("round-canvas") as HTMLCanvasElement;
-    const ctx = canvas?.getContext("2d");
-    if (ctx) {
-      ctx.font = 'bold 24px Gilroy';
-      ctx.fillStyle = '#3b4f7d'; // $text-dark
-      ctx.textAlign = 'center';
-      ctx.textBaseline = 'middle';
-      ctx.fillText("Бариста", canvas.width/2, canvas.height/2);
-    }  
-
     const idInterval = setInterval( ()=> {
       if (this.state.timer > 0 ) {
         this.setState( { timer: this.state.timer - 1, timerLimit: 60 });
@@ -40,8 +30,8 @@ export class RoundProcess extends React.Component {
       <main>
         <div className="round">
           <div className="round__result">+13</div>
-          <div className="round__stage">
-            <canvas id="round-canvas" className="round__card" width="420" height="250"/>
+          <div className="round__stages">
+            <CanvasComponent />
           </div>
 
           <div className="round__buttons">
