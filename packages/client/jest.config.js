64dd21e97ad8c94e39d8777d96416504e1fb0aae
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2022-10-17 23:09:56 +0300

    Jest transform stub update

--- packages/client/jest.config.js
+++ packages/client/jest.config.js
@@ -6,7 +6,7 @@ export default {
   testEnvironment: 'jsdom',
   testMatch: ['<rootDir>/src/**/?(*.)test.{ts,tsx}'],
   moduleNameMapper: {
-    '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$': 'jest-transform-stub'
+    '^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2|svg)$': 'jest-transform-stub'
   },
   globals: {
     __SERVER_PORT__: process.env.SERVER_PORT,
