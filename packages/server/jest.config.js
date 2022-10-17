9e7ad712ba348b3a6822b03a7892193c8de43a65
Yaroslav Starushchenko <starushchenkoyaroslav@gmail.com>
2022-10-17 23:04:35 +0300

    Jest transform stub

--- packages/server/jest.config.js
+++ packages/server/jest.config.js
@@ -1,4 +1,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  moduleNameMapper: {
+    "^.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub"
+  }
 }
